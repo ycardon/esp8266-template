a01d3794125df1700b55592f956233204eaeae6e
Yann Cardon <yann.cardon@gmail.com>
2017-07-02 10:46:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# esp8266-template
+
+an esp8266 application template
+
+- spawn a hotspot to get wifi connectivity (WiFiManager)
+- over the air update (ArduinoOTA)
+- remote telnet debug (RemoteDebug)
---
+++ src/esp8266-template.ino
@@ -0,0 +1,48 @@
+/*
+  D1 mini + matrix shield = clock
+*/
+#include <ESP8266WiFi.h>
+#include <RemoteDebug.h>
+#include <ArduinoOTA.h>
+#include <WiFiManager.h>
+
+#include <ESP8266mDNS.h>       // RemoteDebug dependency
+#include <WiFiUdp.h>           // ArduinoOTA dependency
+#include <DNSServer.h>         // WiFiManager dependency
+#include <ESP8266WebServer.h>  // WiFiManager dependency
+
+#define HOST_NAME "esp8266-template"
+
+// --- LIBRARIES INIT ---
+WiFiManager   wifiManager;
+RemoteDebug   debug;
+WiFiUDP       ntpUDP;
+NTPClient     timeClient(ntpUDP);
+
+// --- SETUP ---
+void setup() {
+
+  // create a hotspot to get wifi credentials
+  wifiManager.autoConnect(HOST_NAME);
+
+  // remote debug
+  Serial.begin(9600);
+  debug.begin(HOST_NAME);
+  debug.setResetCmdEnabled(true);
+  debug.setSerialEnabled(true);
+
+  // over the air update
+  ArduinoOTA.setHostname(HOST_NAME);
+  ArduinoOTA.begin();
+}
+
+// --- MAIN LOOP ---
+void loop() {
+
+  // check for updates and remote debug
+  ArduinoOTA.handle();
+  debug.handle();
+
+  // wait 1s
+  delay(1000);
+}
